e249778df5c6ab36809bd813985c4215adb85e2e
Plotnikova Anastasiya <nast_19980728@mail.ru>
2021-10-03 22:24:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketCost = 10_000;
+        int oneMileCost = 20;
+        int result = ticketCost / oneMileCost;
+        System.out.println(result);
+    }
+
+}
